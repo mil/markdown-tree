312f35469b9f6a2bb3ecf3a6820b54ba5e813baf
mil <miles.sandlar@gmail.com>
2015-01-24 10:51:58 -0500

    spaces instead of tabs

--- markdown-tree.rb
+++ markdown-tree.rb
@@ -8,64 +8,64 @@ require 'yaml'
 $config = YAML.load_file('config.yaml')
 
 #Set up Sinatra Config
-set	:views => "#{settings.root}/#{$config['template-folder']}/",
-	:public_folder => "#{settings.root}/#{$config['template-folder']}/",
-	:static => true
+set  :views => "#{settings.root}/#{$config['template-folder']}/",
+:public_folder => "#{settings.root}/#{$config['template-folder']}/",
+:static => true
 
 #All Pages
 get '/*' do
-	path = params[:splat].join.split("/")
+  path = params[:splat].join.split("/")
 
-	#File or folder
-	folder = "#{Dir.getwd}/#{$config['hierarchy-folder']}/#{params[:splat].join}"
-	if File.directory?(folder) then
-		file = "index"
-	else
-		folder.gsub!(/\/[^\/]+$/,"")
-		file = path.pop
-	end
+  #File or folder
+  folder = "#{Dir.getwd}/#{$config['hierarchy-folder']}/#{params[:splat].join}"
+  if File.directory?(folder) then
+    file = "index"
+  else
+    folder.gsub!(/\/[^\/]+$/,"")
+    file = path.pop
+  end
 
-	#Get children of current folder
-	children = getChildren(folder)
+  #Get children of current folder
+  children = getChildren(folder)
 
-	#Render Markdown
-	begin
-		content = File.read("#{folder}/#{file}.#{$config['markdown-extension']}")
-	rescue
-		content = ""
-	end
+  #Render Markdown
+  begin
+    content = File.read("#{folder}/#{file}.#{$config['markdown-extension']}")
+  rescue
+    content = ""
+  end
 
-	erb :template, :locals => {
-		:siteTitle => $config['site-title'], 
+  erb :template, :locals => {
+    :siteTitle => $config['site-title'], 
 
-		:currentFile => file,
-		:path => path,
-		:children => children,
+    :currentFile => file,
+    :path => path,
+    :children => children,
 
-		:content => markdown(content)
-	}
+    :content => markdown(content)
+  }
 end
 
 #Children Array
 def getChildren(folderPath)
-	children = {
-		:directories => [],
-		:pages => []
-	}
+  children = {
+    :directories => [],
+    :pages => []
+  }
 
-	#Loop through each child
-	Dir.foreach(folderPath) do |child|
-		# Skip if index or . or ..
-		next if child.match(/^(index.#{$config['markdown-extension']}|\.+)$/)
+  #Loop through each child
+  Dir.foreach(folderPath) do |child|
+    # Skip if index or . or ..
+    next if child.match(/^(index.#{$config['markdown-extension']}|\.+)$/)
 
-		#Children folder or file
-		if File.directory?("#{folderPath}/#{child}") then 
-			children[:directories].push(child)
-		else
-			#Throw away extension and push
-			children[:pages].push(child.gsub!(/\.[^.]+$/, ''))
-		end	
-	end
+      #Children folder or file
+      if File.directory?("#{folderPath}/#{child}") then 
+        children[:directories].push(child)
+    else
+      #Throw away extension and push
+      children[:pages].push(child.gsub!(/\.[^.]+$/, ''))
+    end  
+  end
 
-	return children
+  return children
 end
